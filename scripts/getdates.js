46105c207a249fa392e9652dc893b89605777743
Mateus <mtsschiavi@gmail.com>
2023-09-23 21:36:29 -0300

    Finished

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,4 +2,4 @@ const currentYear = new Date().getFullYear();
 document.getElementById('Year').textContent = currentYear;
 
 const lastModified = document.lastModified;
-document.getElementById('Modified').textContent = `Last Modified: ${lastModified}`;
+document.getElementById('Modified').textContent = `${lastModified}`;
