c98d1465e214a6f369c889fde8f8e8fb31e54c23
Mateus <mtsschiavi@gmail.com>
2023-09-28 23:40:52 -0300

    almost there

--- scripts/button.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById("menu-toggle");
-const nav = document.querySelector('.nav');
-
-menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('open');
-});
