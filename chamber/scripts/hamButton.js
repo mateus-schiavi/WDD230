1c3c5133f52085d9a34bedbe481e8baa1e7c5c36
Mateus Schiavi <mtsschiavi@gmail.com>
2023-10-14 17:32:42 -0300

    added dark mode

--- chamber/scripts/hamButton.js
+++ chamber/scripts/hamButton.js
@@ -1,7 +1,25 @@
 
-const menuToggle = document.getElementById('menu-toggle');
-const navMenu = document.querySelector('.nav');
+// const menuToggle = document.getElementById('menu-toggle');
+// const navMenu = document.querySelector('.nav');
 
-menuToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('nav-active');
+// menuToggle.addEventListener('click', () => {
+//     navMenu.classList.toggle('nav-active');
+// });
+
+
+const modeButton = document.querySelector("#mode");
+const html = document.querySelector("html");
+const main = document.querySelector("main");
+
+modeButton.addEventListener('click', ()=> {
+     if (modeButton.textContent.includes("🌑")) {
+        html.classList.toggle('dark-mode');
+         main.style.background = "#000";
+         main.style.color = "#fff";
+         modeButton.textContent = "🔆";  
+    } else {
+         main.style.background = "#fff";
+         main.style.color = "#000";
+         modeButton.textContent = "🌑";
+     }
 });
