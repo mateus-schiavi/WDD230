492ddf06bbf9b7425d3ed43ea51e13336b1fdd44
Mateus Schiavi <mtsschiavi@gmail.com>
2023-10-27 17:05:35 -0300

    added favicon and og:image

--- scripts/getWeather.js
+++ scripts/getWeather.js
@@ -1,36 +1,34 @@
-const apiKey = '3b024a287b26713af80a79e82a761e2e';
-
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Suzano&appid=${apiKey}&units=metric`;
-const weatherIconBaseURL = 'https://openweathermap.org/img/w/';
+const apiKey = '08cfe187e90ec9db6c2929365129bbc3';
+const city = 'Suzano';
+const weatherIconBaseURL = 'https://openweathermap.org/img/wn/';
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 function getWeatherData() {
     fetch(apiUrl)
         .then((response) => response.json())
         .then((data) => {
-            const temperatureElement = document.getElementById('temperature');
+            const temperatureElement = document.getElementById('temperature-value');
             const weatherIconElement = document.getElementById('weather-icon');
 
             if (data.main && data.main.temp) {
                 const temperature = data.main.temp;
-                temperatureElement.textContent = temperature.toFixed(1);
-
-                const celsiusSymbol = document.createElement('span');
-                celsiusSymbol.textContent = '°C';
-                temperatureElement.appendChild(celsiusSymbol);
+                temperatureElement.textContent = temperature.toFixed(0) + '°C';
             } else {
                 temperatureElement.textContent = 'N/A';
             }
 
             if (data.weather && data.weather[0] && data.weather[0].icon) {
-                const weatherIconUrl = `${weatherIconBaseURL}${data.weather[0].icon}.png`;
+                const iconCode = data.weather[0].icon;
+                const weatherIconUrl = `${weatherIconBaseURL}${iconCode}@2x.png`;
                 weatherIconElement.src = weatherIconUrl;
             } else {
-                weatherIconElement.src = 'placeholder-icon.png';
+                weatherIconElement.src = 'https://openweathermap.org/img/wn/10d@2x.png';
             }
         })
         .catch((error) => {
-            console.error('An Error Has Occurred', error);
+            console.error('Ocorreu um erro:', error.message);
         });
 }
 
-getWeatherData();
+// Chama a função getWeatherData quando o conteúdo da página estiver carregado.
+document.addEventListener('DOMContentLoaded', getWeatherData);
