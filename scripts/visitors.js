8dfce353f2b5082faafd1b633444badcf6f7fda1
Mateus Schiavi <mtsschiavi@gmail.com>
2023-10-27 17:13:24 -0300

    updated visitors

--- scripts/visitors.js
+++ scripts/visitors.js
@@ -1,20 +1,23 @@
-function incrementVisitorCount() {
-    let count = sessionStorage.getItem('visitorCount');
-    
-    if (count === null) {
-        count = 0;
-    } else {
-        count = parseInt(count);
-    }
-    
-    count++;
-    
-   
-    sessionStorage.setItem('visitorCount', count);
-    
+function updateVisitorCount() {
+    // Verifica se o contador já existe no localStorage.
+    let count = localStorage.getItem('visitorCount');
 
+    if (count === null) {
+        // Se o contador não existir, defina-o como 1 (primeira visita).
+        count = 1;
+    } else {
+        // Se o contador existir, converta-o para um número.
+        count = parseInt(count);
+        // Incremente o contador.
+        count++;
+    }
+
+    // Armazene o contador atualizado no localStorage.
+    localStorage.setItem('visitorCount', count);
+
+    // Atualize o elemento HTML com o contador.
     document.getElementById('visitorCount').textContent = count;
 }
 
-
-incrementVisitorCount();
+// Chame a função para atualizar o contador de visitantes.
+updateVisitorCount();
