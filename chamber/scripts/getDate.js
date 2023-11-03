ab3f02806d28b048f8d0363c6159a7d8f19418e6
Mateus Schiavi <mtsschiavi@gmail.com>
2023-11-03 12:12:19 -0300

    removed blank spaces

--- chamber/scripts/getDate.js
+++ chamber/scripts/getDate.js
@@ -4,7 +4,6 @@ document.getElementById('Year').textContent = currentYear;
 const lastModified = document.lastModified;
 document.getElementById('Modified').textContent = `${lastModified}`;
 
-// Função para criar um calendário de um mês específico
 function createMonthCalendar(year, month) {
     const calendar = document.getElementById("calendar");
     const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
@@ -18,7 +17,6 @@ function createMonthCalendar(year, month) {
     monthHeader.textContent = new Date(year, month).toLocaleString('en-US', { month: 'long' });
     table.appendChild(monthHeader);
 
-    // Cria a linha com os dias da semana
     const headerRow = document.createElement("tr");
     daysOfWeek.forEach(day => {
         const th = document.createElement("th");
@@ -27,7 +25,6 @@ function createMonthCalendar(year, month) {
     });
     table.appendChild(headerRow);
 
-    // Cria as células vazias no início do mês, de acordo com o dia da semana do primeiro dia
     let currentRow = document.createElement("tr");
     for (let i = 0; i < startingDay; i++) {
         const td = document.createElement("td");
@@ -53,49 +50,43 @@ function createMonthCalendar(year, month) {
     calendar.appendChild(table);
 }
 
-// Função para navegar para o mês anterior
 document.getElementById("prev-month").addEventListener("click", () => {
     const currentDate = new Date();
     currentDate.setMonth(currentDate.getMonth() - 1);
     createMonthCalendar(currentDate.getFullYear(), currentDate.getMonth());
 });
 
-// Função para navegar para o próximo mês
 document.getElementById("next-month").addEventListener("click", () => {
     const currentDate = new Date();
     currentDate.setMonth(currentDate.getMonth() + 1);
     createMonthCalendar(currentDate.getFullYear(), currentDate.getMonth());
 });
 
-// Inicialmente, crie o calendário para o mês atual
 const today = new Date();
 createMonthCalendar(today.getFullYear(), today.getMonth());
 
-// Seleciona todos os elementos img no documento
 const images = document.querySelectorAll("img");
 
-// Itera sobre a lista de elementos img e define o atributo loading como "lazy"
 images.forEach((img) => {
   img.setAttribute("loading", "lazy");
 });
 
-// Função para obter a data da última visita do usuário armazenada no armazenamento local
 function getUltimaVisita() {
     const dataVisitaAnterior = localStorage.getItem('ultimaVisita');
     if (dataVisitaAnterior) {
       return new Date(dataVisitaAnterior);
     } else {
-      return null; // Se não houver data de visita anterior, retorna nulo
+      return null;
     }
-  }
-  
-  function verificarIntervaloVisita() {
+}
+
+function verificarIntervaloVisita() {
     const dataAtual = new Date();
     const dataUltimaVisita = getUltimaVisita();
     const visitaMessage = document.getElementById("visit-message");
-  
+
     if (dataUltimaVisita) {
-      const diffDays = Math.floor((dataAtual - dataUltimaVisita) / (1000 * 60 * 60 * 24)); // Calcula a diferença em dias
+      const diffDays = Math.floor((dataAtual - dataUltimaVisita) / (1000 * 60 * 60 * 24));
       if (diffDays < 1) {
         visitaMessage.textContent = "Welcome Back!";
       } else {
@@ -104,11 +95,8 @@ function getUltimaVisita() {
     } else {
       visitaMessage.textContent = "This is your first visit!";
     }
-  
-    // Armazena a data atual como a nova data de última visita
+
     localStorage.setItem('ultimaVisita', dataAtual);
-  }
-  
-  // Chame a função para verificar o intervalo de visita
-  verificarIntervaloVisita();
-  
+}
+
+verificarIntervaloVisita();
