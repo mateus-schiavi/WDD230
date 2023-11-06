2642ee051af3fb10745196e51dd9c8f1bc4eea00
Mateus Schiavi <mtsschiavi@gmail.com>
2023-11-06 12:39:56 -0300

    fixed

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -7,9 +7,12 @@ document.addEventListener("DOMContentLoaded", function() {
         const emailValue = document.getElementById("email").value;
         const phoneValue = document.getElementById("phone").value;
         const commentsValue = document.getElementById("comments").value;
-
+        const addressValue = document.getElementById("address").value;
+        const numberValue = document.getElementById("number").value;
+        const surnameValue = document.getElementById("surname").value;
+        
         // Verifique se algum dos campos está vazio
-        if (nameValue === "" || emailValue === "" || phoneValue === "" || commentsValue === "") {
+        if (nameValue === "" || emailValue === "" || phoneValue === "" || commentsValue === "" || addressValue === "" || numberValue === "" || surnameValue === "") {
             // Exiba uma mensagem de erro
             alert("Please fulfill all the fields to join us.");
             event.preventDefault(); // Impede o envio do formulário
