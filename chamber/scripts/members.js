197fb60f732de81d7abf718bb728fec14f1bba1a
Mateus Schiavi <mtsschiavi@gmail.com>
2023-11-14 18:56:08 -0300

    started directory image

---
+++ chamber/scripts/members.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const businessContainer = document.getElementById("business-container");
+
+    fetch("../../../wdd230/chamber/data/members.json")
+        .then(response => response.json())
+        .then(data => {
+            const businesses = data.businesses;
+
+            businesses.forEach(function (business) {
+                const div = document.createElement("div");
+                div.classList.add("business");
+
+                const img = document.createElement("img");
+                img.src = business.image;
+                img.alt = business.name;
+
+                const name = document.createElement("h2");
+                name.textContent = business.name;
+
+                const phone = document.createElement("p");
+                phone.textContent = "Phone: " + business.phone;
+
+                const address = document.createElement("p");
+                address.textContent = "Address: " + business.address;
+
+                const website = document.createElement("a");
+                website.textContent = "Website";
+                website.href = business.website;
+                website.target = "_blank";
+
+                const membership = document.createElement("p");
+                membership.textContent = "Membership Level: " + business.membershiplevel;
+
+                div.appendChild(img);
+                div.appendChild(name);
+                div.appendChild(phone);
+                div.appendChild(address);
+                div.appendChild(website);
+                div.appendChild(membership);
+
+                businessContainer.appendChild(div);
+            });
+        })
+        .catch(error => {
+            console.log("Error fetching data:", error);
+        });
+});
